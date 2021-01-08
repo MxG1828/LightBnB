adf184772a760168775fdf60d874422e2d834018
Michael Xiang <yorkmike4@gmail.com>
2021-01-08 09:01:36 +0000

    add property

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -83,7 +83,7 @@ const getAllProperties = function (options, limit = 10) {
   let query = `
   SELECT properties.*, AVG(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
   
   if (options.city){
@@ -124,7 +124,6 @@ const getAllProperties = function (options, limit = 10) {
   query += `
   ORDER BY cost_per_night
   LIMIT $${param.length}`;
-  console.log(query,param);
   return pool.query(query,param)
     .then((res) => res.rows);
 };
@@ -136,9 +135,20 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let param = [property.owner_id, property.title, property.discription, property.thumbnail_photo_url, property.cover_photo_url,
+    property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms,
+    property.number_of_bedrooms];
+  let query = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+    cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms,
+    number_of_bedrooms)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+  
+  return pool.query(query,param)
+  .then(res => {
+    console.log (res.rows)
+    return res.rows})
 };
 exports.addProperty = addProperty;
